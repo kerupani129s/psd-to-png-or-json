bb55ab52e8ad85ef7b63769129575f35ac96c222
ケルパニ＠猫 <32389989+kerupani129s@users.noreply.github.com>
2022-04-22 18:16:27 +0900

    update: キャッシュ対策をハッシュ値に変更。ただしサードパーティ製ライブラリを除く

---
+++ update-cache-busting.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -euoo pipefail posix
+
+# 
+readonly PSD_JS_LIB_PARAM='v3.4.0'
+readonly JSZIP_LIB_PARAM='v3.7.1'
+
+# 
+function content_hash() {
+	local -r file="$1"
+	openssl md4 "$file" | awk '{ print substr($NF, 0, 20) }'
+	return 0
+}
+
+# 
+SITE_CSS_PARAM="$(content_hash ./docs/site.css)"
+readonly SITE_CSS_PARAM
+
+CONVERTER_MAIN_CSS_PARAM="$(content_hash ./docs/main.css)"
+readonly CONVERTER_MAIN_CSS_PARAM
+CONVERTER_MAIN_JS_PARAM="$(content_hash ./docs/main.js)"
+readonly CONVERTER_MAIN_JS_PARAM
+
+VIEWER_MAIN_CSS_PARAM="$(content_hash ./docs/viewer/main.css)"
+readonly VIEWER_MAIN_CSS_PARAM
+VIEWER_VIEWER_JS_PARAM="$(content_hash ./docs/viewer/viewer.js)"
+readonly VIEWER_VIEWER_JS_PARAM
+VIEWER_MAIN_JS_PARAM="$(content_hash ./docs/viewer/main.js)"
+readonly VIEWER_MAIN_JS_PARAM
+
+# 
+sed -Ei \
+	-e 's/(\/site\.css\?)[^"]*/\1'"$SITE_CSS_PARAM"'/g' \
+	-e 's/(\/main\.css\?)[^"]*/\1'"$CONVERTER_MAIN_CSS_PARAM"'/g' \
+	-e 's/(\/psd\.min\.js\?)[^"]*/\1'"$PSD_JS_LIB_PARAM"'/g' \
+	-e 's/(\/jszip\.min\.js\?)[^"]*/\1'"$JSZIP_LIB_PARAM"'/g' \
+	-e 's/(\/main\.js\?)[^"]*/\1'"$CONVERTER_MAIN_JS_PARAM"'/g' \
+	./docs/index.html ./docs/ja/index.html
+
+sed -Ei \
+	-e 's/(\/site\.css\?)[^"]*/\1'"$SITE_CSS_PARAM"'/g' \
+	-e 's/(\/main\.css\?)[^"]*/\1'"$VIEWER_MAIN_CSS_PARAM"'/g' \
+	-e 's/(\/viewer\.js\?)[^"]*/\1'"$VIEWER_VIEWER_JS_PARAM"'/g' \
+	-e 's/(\/main\.js\?)[^"]*/\1'"$VIEWER_MAIN_JS_PARAM"'/g' \
+	./docs/viewer/index.html ./docs/ja/viewer/index.html
+
+sed -Ei \
+	-e 's/(\/site\.css\?)[^"]*/\1'"$SITE_CSS_PARAM"'/g' \
+	./docs/license/index.html ./docs/ja/license/index.html \
+	./docs/third-party-licenses/index.html ./docs/ja/third-party-licenses/index.html
+
+# 
+echo 'OK'
